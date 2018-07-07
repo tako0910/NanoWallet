5426ed4d675c3437f665bdadf21bf36c992b395c
tako0910 <tako.oct0910@gmail.com>
2018-07-07 14:58:32 +0900

    TREZOR: Update connect.js
    from https://github.com/trezor/connect/commit/69db66426ba34f076fc75b0285a17e64db999d3c

--- src/vendors/connect.js
+++ src/vendors/connect.js
@@ -3,7 +3,7 @@
  *
  * GPLv3
  */
-var VERSION = 4;
+var TREZOR_CONNECT_VERSION = 4;
 
 if (!Array.isArray) {
     Array.isArray = function(arg) {
@@ -36,13 +36,8 @@ this.TrezorConnect = (function () {
     var DISABLE_LOGIN_BUTTONS = window.TREZOR_DISABLE_LOGIN_BUTTONS || false;
     var CHROME_URL = window.TREZOR_CHROME_URL || './chrome/wrapper.html';
     var POPUP_ORIGIN = window.TREZOR_POPUP_ORIGIN || 'https://connect.trezor.io';
-    var POPUP_PATH = window.TREZOR_POPUP_PATH || POPUP_ORIGIN + '/' + VERSION;
-    if (window.location.hostname === 'localhost' && !window.TREZOR_POPUP_ORIGIN) {
-        // development settings
-        POPUP_ORIGIN = window.location.origin;
-        POPUP_PATH = POPUP_ORIGIN;
-    }
-    var POPUP_URL = window.TREZOR_POPUP_URL || POPUP_PATH + '/popup/popup.html';
+    var POPUP_PATH = window.TREZOR_POPUP_PATH || POPUP_ORIGIN + '/' + TREZOR_CONNECT_VERSION;
+    var POPUP_URL = window.TREZOR_POPUP_URL || POPUP_PATH + '/popup/popup.html?v=' + new Date().getTime();
 
     var POPUP_INIT_TIMEOUT = 15000;
 
